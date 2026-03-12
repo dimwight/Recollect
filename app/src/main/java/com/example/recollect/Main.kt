37417baf97739e6d4b316bf26afeb4c351f99b5b
dimwight <dimwight@hotmail.com>
2026-03-12 15:05:48 +0000

    Scaffold

--- app/src/main/java/com/example/recollect/Main.kt
+++ app/src/main/java/com/example/recollect/Main.kt
@@ -3,15 +3,10 @@ package com.example.recollect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,18 +29,9 @@ class Main : ComponentActivity() {
         }
         println("R1: formDef = $formDef")
 
-
-
-        enableEdgeToEdge()
         setContent {
             RecollectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    BackNext()
-                }
+                ScaffoldSet()
             }
         }
     }
@@ -69,18 +55,3 @@ fun BackNext() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-//        modifier = Modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecollectTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/recollect/Scaffold.kt
@@ -0,0 +1,90 @@
+package com.example.recollect
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun newTopBar() {
+    TopAppBar(
+        colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text("Top app bar")
+        }
+    )
+}
+@Composable
+fun newBottomBar() {
+    BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.primary,
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "Bottom app bar",
+        )
+    }
+}
+@Composable
+fun newContent(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier.padding(innerPadding),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text =
+                """
+                This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+                
+                It also contains some basic inner content, such as this text.
+                
+                You have pressed the floating action button presses times.
+                     
+                """.trimIndent(),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ScaffoldSet() {
+    Scaffold(
+        topBar = { newTopBar() },
+        bottomBar = { newBottomBar() }
+    ) { innerPadding -> newContent(innerPadding) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
