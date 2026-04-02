5801095d68498c9c6dacd39507d9c5bb244519e5
dimwight <dimwight@hotmail.com>
2026-04-02 22:36:41 +0100

    IME

--- app/src/main/java/com/example/recollect/bits/ImeChecks.kt
+++ app/src/main/java/com/example/recollect/bits/ImeChecks.kt
@@ -1,9 +1,11 @@
 package com.example.recollect.bits
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fitInside
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.Button
@@ -12,9 +14,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.RectRulers
 import androidx.compose.ui.layout.WindowInsetsRulers
 import androidx.compose.ui.layout.innermostOf
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ImeCheck(insideNotPadding: Boolean) {
@@ -51,10 +55,17 @@ fun ImeCheck(insideNotPadding: Boolean) {
             // Trigger
             TextField("Click here!", {})
             // Bad checks?
-            if (true) Text(caption, alignAtBottom)
-            else Button(onClick = {}, alignAtBottom) {
+            if (false) Text(caption, alignAtBottom)
+            else if (false)
+                Button(onClick = {}, alignAtBottom) {
                 Text(caption)
             }
+            else
+                Box(alignAtBottom
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(Color(0xFF53D9A1))
+                )
         }
     }
 }
