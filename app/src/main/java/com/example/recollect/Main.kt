13cdad34659060cf0fba24f5e2e6b0e492f496a2
dimwight <dimwight@hotmail.com>
2026-04-06 15:42:09 +0100

    Ime

--- app/src/main/java/com/example/recollect/Main.kt
+++ app/src/main/java/com/example/recollect/Main.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.recollect.bits.Ime
 import com.example.recollect.bits.Show
 import com.example.recollect.ui.theme.RecollectTheme
 import kotlinx.coroutines.delay
@@ -72,7 +73,7 @@ class Main : ComponentActivity() {
         setContent {
             RecollectTheme {
                 if (false) FormPage(questionDetails)
-                else Show()
+                else Ime()
             }
         }
 
